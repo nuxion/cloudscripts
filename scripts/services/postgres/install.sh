6f7fd1a817c10b535e88f90d4238cc1a73e50f04
nuxion <nuxion@gmail.com>
2022-08-03 10:46:18 -0300

    adding services

--- scripts/services/postgres/install.sh
+++ scripts/services/postgres/install.sh
@@ -2,18 +2,22 @@
 source $BASEDIR/commands/docker.sh
 source $BASEDIR/commands/gomplate.sh
 service_name="${postgres}"
+version="14.2-alpine"
+service_path="/opt/services/${service_name}_${version}"
 
 deploy(){
-    export POSTGRES_VERSION=${1:-latest}
+    export POSTGRES_VERSION="${version}"
     export POSTGRES_ADDRESS=$CLOUD_IPV4_PRIV
-    export POSTGRES_PORT=5432
-    service_path="/opt/services/${service_name}_${POSTGRES_VERSION}"
-    gomplate -d config=./config.yaml -f templates/docker-compose.yaml > rendered/docker-compose.yaml
-    mkdir -p "${service_path}"
-    mkdir -p "${service_path}/scripts"
-    mv rendered/docker-compose "${service_path}/docker-compose.yaml"
-    cp files/createpsql "${service_path}/scripts/"
-    cp files/sqldump "${service_path}/scripts/"
-    cd "${service_path}"
-    docker-compose up -d
+    #export POSTGRES_PORT=5432
+    gomplate -d config="${MAIN_CONFIG}" -f templates/docker-compose.yaml > rendered/docker-compose.yaml
+    mkdir -p "${service_path}_${version}"
+    mkdir -p "${service_path}_${version}/scripts"
+    mv rendered/docker-compose "${service_path}_${version}/docker-compose.yaml"
+    cp files/createpsql "${service_path}_${version}/scripts/"
+    cp files/sqldump "${service_path}_${version}/scripts/"
+}
+
+up(){
+   cd "${service_path}_${version}"
+   docker-compose up -d
 }
