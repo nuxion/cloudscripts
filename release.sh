0d20cb394dc546da058f1eb98278ec3966a004c2
nuxion <nuxion@gmail.com>
2022-10-23 20:38:54 -0300

    fix release process

--- release.sh
+++ release.sh
@@ -1,17 +1,7 @@
-#!/bin/sh
-ensure_var()
-{
-    if [[ -z "${!1:-}" ]];
-    then
-        echo "${1} is unset"
-        exit 1
-    else
-        echo "${1} is ${!1}"
-    fi
-}
+#!/bin/bash
 
+VERSION=$1
 
-ensure_var VERSION
 sed -i "/[[:space:]]VERSION/c\    VERSION=${VERSION}" install.sh
 sed -i "/^VERSION/c\VERSION=${VERSION}" scripts/cscli
 echo "Versions changed to ${VERSION}"
